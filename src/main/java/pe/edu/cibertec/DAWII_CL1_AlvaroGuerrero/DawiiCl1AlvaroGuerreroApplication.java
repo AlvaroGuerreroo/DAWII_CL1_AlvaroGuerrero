68e9ac2f3d882fc37c51dcf11bc3613cecfa0ac8
alvaro <alvaro@hotmail.com>
2023-05-07 05:47:07 -0500

    Initial commit

---
+++ src/main/java/pe/edu/cibertec/DAWII_CL1_AlvaroGuerrero/DawiiCl1AlvaroGuerreroApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DAWII_CL1_AlvaroGuerrero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiiCl1AlvaroGuerreroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawiiCl1AlvaroGuerreroApplication.class, args);
+	}
+
+}
